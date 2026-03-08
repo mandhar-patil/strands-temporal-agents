7399c3d92004bd15a9732a381fa27a9edc98afe7
mandhar patil <patilmandhar42@gmail.com>
2026-03-08 15:19:25 +0530

    Add ai_orchestrator_activity to Docker worker

--- docker_monitor/docker_worker.py
+++ docker_monitor/docker_worker.py
@@ -9,7 +9,12 @@ from temporalio.worker import Worker
 sys.path.insert(0, str(Path(__file__).parent.parent))
 
 from config import TEMPORAL_HOST, DOCKER_MONITOR_TASK_QUEUE
+
+# Import workflow
 from docker_monitor.docker_temporal_agent import DockerMonitorWorkflow
+
+# Import activities
+from docker_monitor.docker_temporal_agent import ai_orchestrator_activity
 
 logging.basicConfig(
     level=logging.INFO,
@@ -31,11 +36,16 @@ async def main():
         client,
         task_queue=DOCKER_MONITOR_TASK_QUEUE,
         workflows=[DockerMonitorWorkflow],
-        activities=[]
+        activities=[ai_orchestrator_activity],  
     )
 
     logger.info("Docker monitor worker started...")
 
+    await worker.run()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
     await worker.run()
 
 
