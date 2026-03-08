f377c96d142d1ce05c7629f7a9430428f9de3e10
mandhar patil <patilmandhar42@gmail.com>
2026-03-08 15:28:48 +0530

    Refactor docker_worker to include new activity

--- docker_monitor/docker_worker.py
+++ docker_monitor/docker_worker.py
@@ -9,12 +9,11 @@ from temporalio.worker import Worker
 sys.path.insert(0, str(Path(__file__).parent.parent))
 
 from config import TEMPORAL_HOST, DOCKER_MONITOR_TASK_QUEUE
-
-# Import workflow
-from docker_monitor.docker_temporal_agent import DockerMonitorWorkflow
-
-# Import activities
-from docker_monitor.docker_temporal_agent import ai_orchestrator_activity
+from docker_monitor.docker_temporal_agent import (
+    DockerMonitorWorkflow,
+    ai_orchestrator_activity,
+    get_container_status_activity
+)
 
 logging.basicConfig(
     level=logging.INFO,
@@ -36,16 +35,14 @@ async def main():
         client,
         task_queue=DOCKER_MONITOR_TASK_QUEUE,
         workflows=[DockerMonitorWorkflow],
-        activities=[ai_orchestrator_activity],  
+        activities=[
+            ai_orchestrator_activity,
+            get_container_status_activity
+        ],
     )
 
     logger.info("Docker monitor worker started...")
 
-    await worker.run()
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
     await worker.run()
 
 
